bdf27e09426c9ee55c78f625a45d6aefa3c1665a
nwgat <github@nwgat.net>
2016-03-14 01:38:25 +0100

    Update playkodi.sh

--- playkodi.sh
+++ playkodi.sh
@@ -39,7 +39,6 @@ while getopts "u?msl:" opt; do
         ;;
     s)  curl --user "$user" --header "Content-Type: application/json" --data '{"jsonrpc": "2.0", "method": "Player.Stop", "params": {"playerid":1}, "id": 1}' "$host/jsonrpc"
         ;;
-        read file
     l)  curl --user "$user" --header "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"Player.Open","params":{"item": {"file":"'"$file"'"}},"id":1}' "$host/jsonrpc"
         ;;
     esac
